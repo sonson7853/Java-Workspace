f774c3651063910519dde6ffb781c172bc6e03ab
sonson7853 <117968202+sonson7853@users.noreply.github.com>
2022-11-14 14:54:27 +0900

    분기문

--- 03_Control/src/com/kh/chap02/run/Run.java
+++ 03_Control/src/com/kh/chap02/run/Run.java
@@ -5,7 +5,10 @@ public class Run {
 
 	public static void main(String[] args) {
 		A_For af = new A_For();
-		af.method15();
+		//af.method15();
+		
+		B_While bw = new B_While();
+		bw.method7();
 		
 	}
 }
