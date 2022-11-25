2fd1b27109a656d769b77676e564390f47e6dc44
sonson <sonson7853@naver.com>
2022-11-25 15:17:39 +0900

    22.11.25

---
+++ 08_Inheritance/src/src/com/kh/practice/point/controller/RectangleController.java
@@ -0,0 +1,18 @@
+package com.kh.practice.point.controller;
+
+import com.kh.practice.point.model.vo.*;
+
+public class RectangleController {
+	private Rectangle r = new Rectangle();
+	
+	public String calcArea(int x , int y, int height, int width) {
+		r = new Rectangle(x, y, height ,width);
+		return r.toString()+ " / "+ height * width;
+	}
+	
+	public String calcPerimeter(int x, int y, int height, int width) {
+		r = new Rectangle(x , y , height, width);
+		return r.toString()+ " / "+ 2*(width + height);
+	}
+	
+}
---
+++ 09_Polymorphism/src/com/kh/practice01_poly/run/Run.java
@@ -0,0 +1,12 @@
+package com.kh.practice01_poly.run;
+
+import com.kh.practice01_poly.view.LibraryMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		LibraryMenu lm = new LibraryMenu();
+		lm.mainMenu();
+	}
+
+}
